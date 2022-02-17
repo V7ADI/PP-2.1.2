300252c131c1c3598228d2e37e53ab7a90799d4f
V7ADI <therion.it@gmail.com>
2022-02-17 20:16:06 +0400

    commit 1
    commit 1

---
+++ src/main/java/App.java
@@ -0,0 +1,22 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        System.out.println(bean.getMessage() + " Hashcode : " + bean);
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage() + " Hashcode : " + bean2);
+        System.out.println(bean == bean2);
+        Cat bean3 = (Cat) applicationContext.getBean("Cat");
+        System.out.println(bean3.getMessage() + " Hashcode : " + bean3);
+        Cat bean4 = (Cat) applicationContext.getBean("Cat");
+        System.out.println(bean4.getMessage() + " Hashcode : " + bean4);
+        System.out.println(bean3 == bean4);
+    }
+}
